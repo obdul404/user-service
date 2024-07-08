9d165c7e3ad45b8a42e35f643c46847a79ad2bea
obdul404 <108807145+obdul404@users.noreply.github.com>
2024-07-09 02:12:04 +0500

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,52 @@
+# main.py
+from contextlib import asynccontextmanager
+from typing import Union, Optional, Annotated
+from app import settings
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Sequence
+from fastapi import FastAPI, Depends
+from typing import AsyncGenerator
+from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
+import asyncio
+import json
+
+
+# only needed for psycopg 3 - replace postgresql
+# with postgresql+psycopg in settings.DATABASE_URL
+connection_string = str(settings.DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg"
+)
+
+
+# recycle connections after 5 minutes
+# to correspond with the compute scale down
+engine = create_engine(
+    connection_string, connect_args={}, pool_recycle=300
+)
+
+#engine = create_engine(
+#    connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
+#)
+
+
+
+# The first part of the function, before the yield, will
+# be executed before the application starts.
+# https://fastapi.tiangolo.com/advanced/events/#lifespan-function
+# loop = asyncio.get_event_loop()
+@asynccontextmanager
+async def lifespan(app: FastAPI)-> AsyncGenerator[None, None]:
+    print("starting async tasks")
+    yield
+
+app = FastAPI(lifespan=lifespan, title="Hello World API with DB", 
+    version="0.0.1",
+    servers=[
+        {
+            "url": "http://127.0.0.1:8000", # ADD NGROK URL Here Before Creating GPT Action
+            "description": "Development Server"
+        }
+        ])
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
